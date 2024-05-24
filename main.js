ed48f07a5eebafc91ea3685a93b05f4a737c3e78
JoaoCoquim <joaocoquim@gmail.com>
2024-05-24 18:34:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+// store the piano keys
+const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+const notes = [];
+keys.forEach(function(key){
+  notes.push(document.getElementById(key));
+})
+
+const keyPlay = (event) => {
+  if(event.target.className === 'key' || event.target.className === 'black-key'){
+    event.target.style.backgroundColor = 'red';
+  }
+  const audioElement = event.target.querySelector('audio');
+  if(audioElement){
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+}
+
+const keyReturn = (event) => {
+  if(event.target.className === 'key' || event.target.className === 'black-key'){
+    event.target.style.backgroundColor = '';
+  }
+}
+
+const keyStroke = (note) => {
+  note.addEventListener('mousedown', keyPlay);
+  note.addEventListener('mouseup', keyReturn);
+}
+
+// loop that runs the array elements through the function
+notes.forEach(note => keyStroke(note));
+
+// store the buttons that progress the user through the lyrics
+let nextOne = document.getElementById('first-next-line');
+let nextTwo = document.getElementById('second-next-line');
+let nextThree = document.getElementById('third-next-line');
+let startOver = document.getElementById('fourth-next-line');
+let lastLyric = document.getElementById('column-optional');
+
+// "hiding" all progress buttons, but the first one
+nextTwo.hidden = true;
+nextThree.hidden = true;
+startOver.hidden = true;
+
+// event handler property and function for the first progress button
+nextOne.onclick = function() {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+  document.getElementById('letter-note-five').innerHTML = 'G';
+  document.getElementById('letter-note-six').innerHTML = 'F';
+}
+
+// event handler property and function for the second progress button
+nextTwo.onclick = function() {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('word-six').innerHTML = 'FRI-';
+  lastLyric.style.display = 'inline-block';
+  document.getElementById('letter-note-three').innerHTML = 'C2';
+  document.getElementById('letter-note-four').innerHTML = 'A';
+  document.getElementById('letter-note-five').innerHTML = 'F';
+  document.getElementById('letter-note-six').innerHTML = 'E';
+}
+
+// event handler property and function for the third progress button
+nextThree.onclick = function() {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'A#';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'A#';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'A';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'F';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('letter-note-five').innerHTML = 'G';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-six').innerHTML = 'F';
+  lastLyric.style.display = 'none';
+}
+
+// event handler property and function for the startOver button
+startOver.onclick = function() {
+  nextOne.hidden = false;
+  startOver.hidden = true;
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'F';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'F';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'E';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'C';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('word-six').innerHTML = 'YOU';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+}
